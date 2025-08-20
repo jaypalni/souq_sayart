0df8a08a1933e572d9edb014ceadd4557c82e0ab
jaypalni <jayavardhan.g@palni.com>
2025-08-20 10:13:11 +0530

    sonar issues fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 /**
- * Copyright (c) 2025 Palni
+ * @file App.js
+ * @description Root component for the ss-frontend project. Wraps the application with Redux Provider and initializes routing.
+ * @version 1.0.0
+ * @date 2025-08-19
+ * @author Palni
+ *
+ * Copyright (c) 2025 Palni.
  * All rights reserved.
  *
  * This file is part of the ss-frontend project.
- * Unauthorized copying or distribution of this file,
- * via any medium is strictly prohibited.
+ * Unauthorized copying, modification, or distribution of this file,
+ * via any medium is strictly prohibited unless explicitly authorized.
  */
 
 import React from 'react';
@@ -12,6 +18,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import AppRouter from './router';
 
+/**
+ * Root App Component
+ *
+ * @returns {JSX.Element} The wrapped application
+ */
 function App() {
   return (
     <Provider store={store}>
